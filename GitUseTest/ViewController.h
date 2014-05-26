9776c7ed126a50d1c402d8d886ad51bea4a212c4
Yihe Li <yihe@sketch-off.com>
2014-05-26 19:16:33 +0800

    Initial Commit

---
+++ GitUseTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitUseTest
+//
+//  Created by xxx on 14-5-26.
+//  Copyright (c) 2014年 NOVAPEX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GitUseTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitUseTest
+//
+//  Created by xxx on 14-5-26.
+//  Copyright (c) 2014年 NOVAPEX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
