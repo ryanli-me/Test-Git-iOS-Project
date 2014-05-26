ee4ecd70a7696e382f5163b8e5ff9ff3ee215781
Yihe Li <yihe@sketch-off.com>
2014-05-26 19:24:34 +0800

    New View Controller added

---
+++ GitUseTest/NewViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewViewController.h
+//  GitUseTest
+//
+//  Created by xxx on 14-5-26.
+//  Copyright (c) 2014年 NOVAPEX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewViewController : UIViewController
+
+@end
